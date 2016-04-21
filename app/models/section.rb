76ce7e90b3083c13ca7e1048b150e4cac77e73fa
Zaid Al Lahham <zlahham@gmail.com>
2016-04-21 11:02:59 +0300

    add bcrypt

--- app/models/section.rb
+++ app/models/section.rb
@@ -3,6 +3,13 @@ class Section < ActiveRecord::Base
   has_many :section_edits
   has_many :editors, :through => :section_edits, class_name: "AdminUser"
 
+  CONTENT_TYPES = ['text', 'HTML']
+
+  validates_presence_of :name
+  validates_presence_of :name, maximum: 255
+  validates_inclusion_of :content_type, in: CONTENT_TYPES, message: "must be one of: #{CONTENT_TYPES.join(', ')}"
+  validates_presence_of :content
+
   scope :visible,      -> { where(visible: true) }
   scope :invisible,    -> { where(visible: false) }
   scope :sorted,       -> { order("sections.position ASC") }
