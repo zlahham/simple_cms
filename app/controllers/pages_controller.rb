b76bcaf391b17c5fd4f12ee8d11a70360c5976c0
Zaid Al Lahham <zlahham@gmail.com>
2016-04-21 17:22:31 +0300

    final ch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
+
   layout 'admin'
 
   before_action :confirm_logged_in
+  before_action :find_subject
 
   def index
-    @pages = Page.sorted
+    #@pages = Page.where(subject_id: @subject.id).sorted
+    @pages = @subject.pages.sorted
   end
 
   def show
@@ -12,7 +15,7 @@ class PagesController < ApplicationController
   end
 
   def new
-    @page = Page.new({ name: 'Default' })
+    @page = Page.new({ subject_id: @subject.id,name: 'Default' })
     @subjects = Subject.order('position ASC')
     @page_count = Page.count + 1
   end
@@ -21,7 +24,7 @@ class PagesController < ApplicationController
     @page = Page.new(page_params)
     if @page.save
       flash[:notice] = "Page created successfully"
-      redirect_to(action: 'index')
+      redirect_to(action: 'index', subject_id: @subject.id)
     else
       render('new')
       @subjects = Subject.order('position ASC')
@@ -39,7 +42,7 @@ class PagesController < ApplicationController
     @page = Page.find(params[:id])
     if @page.update_attributes(page_params)
       flash[:notice] = "Page updated successfully"
-      redirect_to(action: 'show', id: @page.id)
+      redirect_to(action: 'show', id: @page.id, subject: @subject.id)
     else
       render('edit')
       @subjects = Subject.order('position ASC')
@@ -54,13 +57,19 @@ class PagesController < ApplicationController
   def destroy
     page = Page.find(params[:id]).destroy
     flash[:notice] = "Page destroyed successfully."
-    redirect_to(action: 'index')
+    redirect_to(action: 'index', subject_id: @subject.id)
   end
 
   private
 
   def page_params
     params.require(:page).permit(:subject_id, :name, :permalink, :position, :visible)
+  end
+
+  def find_subject
+    if params[:subject_id]
+      @subject = Subject.find(params[:subject_id])
+    end
   end
 
 end
